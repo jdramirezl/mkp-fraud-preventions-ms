b57941841dfb13a25dcbae3fe806e7e8574f849b
julianramire_meli <juliandavid.ramirez@mercadolibre.com.co>
2025-05-24 12:16:18 -0500

    chore: Add IAM policy to make Cloud Run service public by allowing all users to invoke it, enhancing accessibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,3 +118,20 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
+# Make the service public
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = var.project_id
+  service     = google_cloud_run_service.default.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
